609b75cb4ef82dd5e853259393e20b65a384a3b2
asgharali97 <asgharalifs48@gmail.com>
2025-10-20 03:19:58 +0500

    added the middlewares in clerk

---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import { authMiddleware, clerkClient } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const publicRoutes = ["/", "api/webhook/register", "/sign-in", "/sign-up"];
+
+export default authMiddleware({
+  publicRoutes,
+  async AfterAuth(auth, req) {
+    if (!auth.userId && !publicRoutes.includes(req.nextUrl.pathname)) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+
+    if (auth.userId) {
+      try {
+        const user = await clerkClient.users.getUser(auth.userId);
+        const role = user.publicMetadata.role as string | undefined;
+
+        if (role === "admin" && req.nextUrl.pathname === "/dashboard") {
+          return NextResponse.redirect(new URL("/admin/dashboard"), req.url);
+        }
+
+        if (role !== "admin" && req.nextUrl.pathname.startWith("/admin")) {
+          return NextResponse.redirect(new URL("/dasborad"), req.url);
+        }
+
+        if (publicRoutes.includes(req.nextUrl.pathname)) {
+          return NextResponse.redirect(
+            role === "admin" ? "admin/dashboard" : "/dashboard",
+            req.url
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching user data from Clerk:", error);
+        return NextResponse.redirect(new URL("/error", req.url));
+      }
+    }
+  },
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
